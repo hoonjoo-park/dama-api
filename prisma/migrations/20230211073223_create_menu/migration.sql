cd9902a2622c639e2e7185f79452b0126548efdf
hoonjoo94 <hoonjoo94@naver.com>
2023-02-11 17:08:44 +0900

    MySQL에 스키마 마이그레이션

---
+++ prisma/migrations/20230211073223_create_menu/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `Menu` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `price` INTEGER NOT NULL,
+    `imageUrl` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
