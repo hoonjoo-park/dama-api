49b65301bb28e36e87a7ff4e5f1706c726ab4abb
hoonjoo94 <hoonjoo94@naver.com>
2023-02-11 22:40:36 +0900

    모든 메뉴 불러올 수 있는 GET/menu 엔드포인트 구현

--- src/menu.ts
+++ src/menu.ts
@@ -4,6 +4,16 @@ import prisma from "../prisma/prisma";
 export const menuUrl = "/menu";
 const menuRouter = Router();
 
+menuRouter.get("/", async (req: Request, res: Response) => {
+  try {
+    const menus = await prisma.menu.findMany();
+
+    res.status(200).json({ menus });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 menuRouter.post("/", async (req: Request, res: Response) => {
   try {
     const { name, price, imageUrl } = req.body;
