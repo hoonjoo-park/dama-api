940e12801b98e262c53e0f18b3aed7622d7c7641
hoonjoo94 <hoonjoo94@naver.com>
2023-02-12 23:11:12 +0900

    morgan 설치 및 적용

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 import menuRouter, { menuUrl } from "./menu";
 
 const app = express();
@@ -7,5 +8,11 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`🦁 Server is running on ${PORT}`));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+} else {
+  app.use(morgan("dev"));
+}
 
 app.use(menuUrl, menuRouter);
